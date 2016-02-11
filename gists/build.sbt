4e4118702bbd2b1f7a24e55057e5f49e7281a6a6
Andrea <andrea.peruffo1982@gmail.com>
2016-02-11 17:50:37 +0100

    removed unused model file

--- gists/build.sbt
+++ gists/build.sbt
@@ -1,11 +1,11 @@
 
-name := "treeview"
+name := "gists"
 
 organization := "eu.unicredit"
 
 scalaVersion := "2.11.7"
 
-scalacOptions := Seq("-feature")
+scalacOptions := Seq("-feature", "-language:_")
 
 enablePlugins(ScalaJSPlugin)
 
@@ -14,8 +14,8 @@ version := "0.1-SNAPSHOT"
 enablePlugins(ScalaJSPlugin)
 
 libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.8.1",
-  "eu.unicredit" %%% "paths-scala-js" % "0.4.2"
+  "akka.js" %%% "akkaactor" % "0.1.0-SNAPSHOT",
+  "org.scala-js" %%% "scalajs-dom" % "0.8.1"
 )
 
 jsDependencies ++= Seq()
---
+++ gists/src/main/scala/eu/unicredit/gists/Main.scala
@@ -0,0 +1,83 @@
+package eu.unicredit.gists
+
+import scala.concurrent.duration._
+import akka.actor._
+
+import scala.scalajs.js
+import js.annotation.JSExport
+
+import js.Dynamic.literal
+import js.JSON
+
+import org.scalajs.dom.ext.Ajax
+import scala.concurrent.ExecutionContext.Implicits.global
+
+@JSExport("Main")
+object Main extends js.JSApp {
+  import GistMsgs._
+
+  val system = ActorSystem("gists")
+
+  val manager = system.actorOf(Props(HandshakeManager()))
+
+  def main() = {
+    println("starting...")
+  }
+
+  @JSExport
+  var username = ""
+
+  @JSExport
+  var password = ""
+
+  @JSExport
+  def publish() = {
+    println("username -> "+username)
+    println("password -> "+password)
+
+    manager ! Publish("")
+  }
+
+  @JSExport
+  def listen() =
+    manager ! Listen
+
+
+  
+  case class HandshakeManager() extends Actor {
+
+    def newUUID() =
+      java.util.UUID.randomUUID().toString
+
+    def newGistActor() =
+      context.actorOf(Props(GistActor(
+        js.Dynamic.global.btoa(username + ":" + password).toString
+        )))
+
+    def receive = {
+      case msg: Publish =>
+        val uuid = newUUID()
+        println("publishing offer "+uuid)
+        newGistActor ! Publish("this is an offer "+uuid)
+      case Listen =>
+        newGistActor ! Listen
+
+      //Publisher answer
+      case AvailableAnswer(id, token) =>
+        val originalSender = sender
+        println("I can close connection with token "+token)
+
+        originalSender ! PoisonPill
+      //Listener request
+      case AvailableOffer(id, token) =>
+        val originalSender = sender
+        println("seen offer with token "+token)
+        val uuid = newUUID()
+        println("writing answer "+uuid)
+        originalSender ! AnswerForOffer(id, "this is an answer "+uuid)
+
+        context.system.scheduler.scheduleOnce(10 seconds)(originalSender ! PoisonPill)        
+    }
+  }
+
+}
