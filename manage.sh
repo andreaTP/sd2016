239683f0a64eb31b3b5701d08cc85061d4127e8f
andreaTP <andrea.peruffo1982@gmail.com>
2016-05-30 16:48:05 +0200

    update

---
+++ manage.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+case "$1" in
+  build)
+    docker -D build -t="slide-docker-akkajs:latest" .
+  ;;
+  run)
+    #remember to start nvidia-docker-plugin
+    docker run --rm -i -t -p 4000:4000 -p 8000:8000 "slide-docker-akkajs:latest"
+  ;;
+  *)
+    echo "Usage [ build, run ] "
+    exit 0
+  ;;
+esac
