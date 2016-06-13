b5c6a10b6c5dbaa1fc61cd67efd3de5799bd931b
andreaTP <andrea.peruffo1982@gmail.com>
2016-06-13 13:17:27 +0200

    indentation

--- code5/jvm/src/main/scala/ChatServerAkkaHttp.scala
+++ code5/jvm/src/main/scala/ChatServerAkkaHttp.scala
@@ -57,7 +57,7 @@ object ChatServerAkkaHttp {
     val route =
       pathSingleSlash {
         get {
-            handleWebSocketMessages(msgFlow)
+          handleWebSocketMessages(msgFlow)
         }
       }
 
